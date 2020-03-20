028d3f1eec5d13aa1a8a8f344562f1756109a249
lordrichard <pauloreizinho@hotmail.com>
2020-03-20 16:46:02 +0100

    close nav onClick correction

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 
 
-class Navigation extends Component {
+class Navigation extends Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+        isChecked: false,
+        };
+    }
+    toggleChange = () => {
+        this.setState({
+        isChecked: !this.state.isChecked,
+        });
+    }
 
     render() {
-        const navListItems = document.getElementsByClassName("navigation__item");
-        for(let i = 0; i < navListItems.length; i++){ 
-            navListItems[i].onclick = () => {
-                document.getElementById("navi-toggle").checked = false;
-            };
-        }
-
         return (
             <div className="navigation">
-                <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
+                <input type="checkbox" className="navigation__checkbox" id="navi-toggle" checked={this.state.isChecked} onChange={this.toggleChange}/>
                 <label for="navi-toggle" className="navigation__button">
                     <span className="navigation__icon">&nbsp;</span>
                 </label>
                 <div className="navigation__background">&nbsp;</div>
                 <nav className="navigation__nav">
                     <ul className="navigation__list">
-                        <li className="navigation__item"><a href="#about" className="navigation__link">About Skytours</a></li>
-                        <li className="navigation__item"><a href="#features" className="navigation__link">Your benefits</a></li>
-                        <li className="navigation__item"><a href="#tours" className="navigation__link">popular tours</a></li>
-                        <li className="navigation__item"><a href="#stories" className="navigation__link">stories</a></li>
-                        <li className="navigation__item"><a href="#book" className="navigation__link">book now</a></li>
+                        <li className="navigation__item"><a href="#about" className="navigation__link" onClick={this.toggleChange} >About Skytours</a></li>
+                        <li className="navigation__item"><a href="#features" className="navigation__link" onClick={this.toggleChange}>Your benefits</a></li>
+                        <li className="navigation__item"><a href="#tours" className="navigation__link" onClick={this.toggleChange}>popular tours</a></li>
+                        <li className="navigation__item"><a href="#stories" className="navigation__link" onClick={this.toggleChange}>stories</a></li>
+                        <li className="navigation__item"><a href="#book" className="navigation__link" onClick={this.toggleChange}>book now</a></li>
                     </ul>
                 </nav>
             </div>
